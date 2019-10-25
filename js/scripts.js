89f81496a5cd0c85a8870e47b93b4a4d77183008
Judah Barton <judah.barton@yahoo.com>
2019-10-25 11:23:05 -0700

    add jQuery variables for the radio buttons

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $("button.submit").click(function(event){
+
+    var ani = parseInt($("input:radio[name=animal]:checked").val());
+    var hol = parseInt($("input:radio[name=holiday]:checked").val());
+    var col = parseInt($("input:radio[name=color]:checked").val());
+    var cel = parseInt($("input:radio[name=celebrity]:checked").val());
+    var mov = parseInt($("input:radio[name=movie]:checked").val());
+    var foo = parseInt($("input:radio[name=food]:checked").val());
+    console.log(ani, hol, col, cel, mov, foo);
+
+  });
+
+
+});
